c51be7964fae574e34a211c94cdb7efb2fc09e21
Nirav Parekh <niruparekh09@gmail.com>
2025-01-22 09:29:52 +0530

    feat: :zap: Completed DLL Implementation

--- 8. Linked List/Implementation/DoublyLinkedList.java
+++ 8. Linked List/Implementation/DoublyLinkedList.java
@@ -19,6 +19,16 @@ public class DoublyLinkedList {
         }
     }
 
+    public static int lengthOfList(Node head) {
+        int cnt = 1;
+        Node temp = head;
+        while (temp.next != null) {
+            cnt++;
+            temp = temp.next;
+        }
+        return cnt;
+    }
+
     public static Node deleteHead(Node head) {
         if (head == null || head.next == null) return null; // In case of no/one elem.
 
@@ -42,11 +52,133 @@ public class DoublyLinkedList {
         return head;
     }
 
+    public static Node deleteKth(Node head, int k) {
+        Node temp = head;
+        int cnt = 0;
+        while (temp != null) {
+            cnt++;
+            if (cnt == k) break;
+            temp = temp.next;
+        }
+
+        Node prevRef = temp.prev;
+        Node nextRef = temp.next;
+
+        //if (prevRef == null) return deleteHead(head); // It's first element
+        //if (nextRef == null) return deleteTail(head); // It's last element
+
+        if (prevRef == null && nextRef == null) return null;
+
+            // In case of First Element
+        else if (prevRef == null) {
+            if (head.next == null) return null;
+
+            Node prev = head;
+            head = head.next;
+            head.prev = null;
+            prev.next = null;
+            return head;
+        }
+        // In case of Last Element
+        else if (nextRef == null) {
+            prevRef.next = null;
+            temp.prev = null;
+            return head;
+        }
+
+        prevRef.next = nextRef;
+        nextRef.prev = prevRef;
+        prevRef = null;
+        nextRef = null;
+        return head;
+    }
+
+    public static void deleteNode(Node node) {
+        if (node == null) return;
+        Node prevRef = node.prev;
+        Node nextRef = node.next;
+        if (nextRef == null) { // It is the tail element
+            prevRef.next = null;
+            node.prev = null;
+            return;
+        }
+
+        prevRef.next = nextRef;
+        nextRef.prev = prevRef;
+        prevRef = null;
+        nextRef = null;
+    }
+
+    public static Node insertBeforeHead(Node head, int n) {
+        Node newNode = new Node(n, head, null);
+        head.prev = newNode;
+        //newNode.next = head;
+        return newNode; // It'll be new head
+    }
+
+    public static Node insertBeforeTail(Node head, int n) {
+        if (head.next == null) return insertBeforeHead(head, n); // In case of single node
+        Node last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        Node secondLast = last.prev;
+        Node newNode = new Node(n, last, secondLast);
+        last.prev = newNode;
+        secondLast.next = newNode;
+        return head;
+    }
+
+    public static Node insertAtKth(Node head, int k, int n) {
+        if (k == 1) return insertBeforeHead(head, n);
+        if (k == lengthOfList(head)) return insertBeforeTail(head, n);
+        Node temp = head;
+        int cnt = 0;
+        while (temp != null) {
+            cnt++;
+            if (cnt == k) break;
+            temp = temp.next;
+        }
+        Node prevEl = temp.prev; // previous to the kth element
+        Node newNode = new Node(n, temp, prevEl);
+        temp.prev = newNode;
+        prevEl.next = newNode;
+        return head;
+    }
+
+    public static Node reverseDLL(Node head) {
+        if (head == null || head.next == null) {
+            // No change is needed;
+            // return the current head
+            return head;
+        }
+        Node current = head; // Current node
+        Node last = null;
+        while (current != null) {
+            last = current.prev;
+            current.prev = current.next;
+            current.next = last;
+            current = current.prev;
+        }
+
+        // Here last will point the second last node of old DLL i.e. the 2nd node of reversed DLL, so we will return
+        // last.prev which will be pointing to the new head.
+        return last.prev;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 4, 5, 6, 7};
         Node head = convertArrToDLL(arr);
         head = deleteHead(head);
         head = deleteTail(head);
+        head = deleteKth(head, 3);
+        deleteNode(head.next.next);
+        head = insertBeforeHead(head, 9);
+        head = insertBeforeTail(head, 12);
+        head = insertAtKth(head, 3, 69);
+        printList(head);
+        System.out.println("\nReversed:");
+        head = reverseDLL(head);
         printList(head);
     }
 
