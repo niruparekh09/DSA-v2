cfe177b2b934334aaee67628fbd10d1fad280a7c
Nirav Parekh <niruparekh09@gmail.com>
2025-01-28 09:35:12 +0530

    feat: :zap: Commit

--- 8. Linked List/LLHard/Merge_K_Sorted_List.java
+++ 8. Linked List/LLHard/Merge_K_Sorted_List.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 public class Merge_K_Sorted_List {
 
@@ -29,16 +28,16 @@ public class Merge_K_Sorted_List {
         return newList;
     }
 
-    public static ListNode mergeKLists(ListNode[] lists) {
+    // TC: O(N^3) SC: O(1)
+    public static ListNode mergeKListsBetter(ListNode[] lists) {
         ListNode head = lists[0];
         for (int i = 1; i < lists.length; i++) {
-            head = mergerLists(head, lists[i]);
+            head = mergeLists(head, lists[i]);
         }
         return head;
     }
 
-    // TC: O(N^3) SC: O(1)
-    public static ListNode mergerLists(ListNode head1, ListNode head2) {
+    public static ListNode mergeLists(ListNode head1, ListNode head2) {
         ListNode dummyNode = new ListNode(-1);
         ListNode t1 = head1;
         ListNode t2 = head2;
@@ -55,6 +54,76 @@ public class Merge_K_Sorted_List {
         }
         if (t1 != null) dummyNode.next = t1;
         else dummyNode.next = t2;
+        return dummyNode.next;
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        // PQ to maintain sorted order on based on node.val
+        PriorityQueue<Map.Entry<Integer, ListNode>> pq = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getKey));
+
+        // Push heads of all the Lists into PQ
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.add(new AbstractMap.SimpleEntry<>(node.val, node));
+            }
+        }
+
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+
+        // This loop will always give the smallest node possible
+        // And we will take that out and then add to dummyNode link
+        while (!pq.isEmpty()) {
+            // Topmost node which will also be the node with smallest val.
+            Map.Entry<Integer, ListNode> smallestNode = pq.poll();
+
+            //Check if the current node has next node
+            ListNode nextNode = smallestNode.getValue().next;
+            if (nextNode != null) {
+                pq.add(new AbstractMap.SimpleEntry<>(nextNode.val, nextNode));
+            }
+
+            temp.next = smallestNode.getValue();
+            temp = temp.next;
+        }
+
+        return dummyNode.next;
+    }
+
+    // Due to error in leetcode we'll directly store ListNode in PQ
+    public ListNode mergeKLists2(ListNode[] lists) {
+        if (lists.length == 0)
+            return null;
+
+        // PriorityQueue to maintain sorted order of nodes based on their value
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.val, b.val));
+
+        // Push the heads of all the lists into the PriorityQueue
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.add(node);
+            }
+        }
+
+        // Dummy node to build the merged linked list
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+
+        // Process the PriorityQueue
+        while (!pq.isEmpty()) {
+            // Extract the smallest node from the PriorityQueue
+            ListNode smallestNode = pq.poll();
+
+            // Add it to the merged linked list
+            temp.next = smallestNode;
+            temp = temp.next;
+
+            // If the extracted node has a next node, push it into the PriorityQueue
+            if (smallestNode.next != null) {
+                pq.add(smallestNode.next);
+            }
+        }
+
         return dummyNode.next;
     }
 
