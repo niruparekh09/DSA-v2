53a02deabbbada690bcd33365976e4c49f262ba2
Nirav Parekh <niruparekh09@gmail.com>
2026-01-17 22:16:40 +0530

    :zap: Refactor

--- 14. Graphs/Introduction/BFS.java
+++ 14. Graphs/Introduction/BFS.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 class BFS {
     public static void main(String[] args) {
--- 14. Graphs/Problems on BFS and DFS/Bipartite.java
+++ 14. Graphs/Problems on BFS and DFS/Bipartite.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Bipartite {
     public static void main(String[] args) {
